29620e3594b356ad717b5a8b79789b0859f0d00b
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-23 22:50:34 +0530

    Create NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setShow(window.scrollY > 100);
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix"
+      />
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="User"
+      />
+    </div>
+  );
+}
+
+export default NavBar;
