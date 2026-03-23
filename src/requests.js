ed4aa3b79456a6cce2f9f5f786587ea1956be5d8
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-03-23 22:50:18 +0530

    Create requests.js

---
+++ src/requests.js
@@ -0,0 +1,11 @@
+const API_KEY = "YOUR_API_KEY";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+};
+
+export default requests;
